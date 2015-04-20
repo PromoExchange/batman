9c420cc0178f28ca280028a56c229e148a5a6b34
tvarley <tim.varley@gmail.com>
2015-04-20 16:10:29 -0400

    Added auction namespace

--- peapi/db/schema.rb
+++ peapi/db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150420115545) do
+ActiveRecord::Schema.define(version: 20150420133659) do
+
+  create_table "auction_categories", force: :cascade do |t|
+    t.string   "name"
+    t.string   "desc"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "product_attributes", force: :cascade do |t|
     t.string   "name"

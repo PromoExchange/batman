e52eb5a7e600d83f86c1454d86ff552b798523bf
tvarley <tim.varley@gmail.com>
2015-04-29 15:18:24 -0400

    Fix api controller namespaces

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,6 +1,12 @@
-class API::V1::ProductsController < API::BaseAPIController
-  private
-    def query_params
-      params.permit(:product_id)
+# product
+module API
+  module V1
+    class ProductsController < API::BaseAPIController
+      private
+
+        def query_params
+          params.permit(:product_id)
+        end
     end
+  end
 end

f9e5b10fb7e8d1cde77caff05569bfdda83e4854
Spencer Applegate <spencer@spencerapplegate.com>
2015-04-30 16:18:29 -0400

    Added devise invitable

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,12 +29,12 @@ class User < ActiveRecord::Base
   after_initialize :set_default_role, if: :new_record?
   rolify
 
+  devise :confirmable, :database_authenticatable, :registerable, :invitable,
+    :recoverable, :rememberable, :trackable, :validatable
+
+  private
+
   def set_default_role
     self.role ||= :user
   end
-
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
 end

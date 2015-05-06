a2c87b1317cf8d6a8a0c1a86acfd05a897d6ede9
tvarley <tim.varley@gmail.com>
2015-05-06 14:30:51 -0400

    Add supplier and product rake tasts

--- config/initializers/upmin_admin.rb
+++ config/initializers/upmin_admin.rb
@@ -1,4 +1,4 @@
 Upmin.configure do |config|
   # Configurations go here - e.g.:
-  config.models = [:user, :product, :category, :supplier]
+  config.models = [:user, :product, :category, :supplier, :color, :material, :size]
 end

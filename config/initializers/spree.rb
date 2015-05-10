bb3b2900817e85b87f08b0476fef4cd9b7af5c98
Spencer Applegate <spencer@spencerapplegate.com>
2015-05-10 12:25:57 -0400

    Saving images to S3

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -18,3 +18,27 @@ end
 Spree.user_class = 'Spree::User'
 
 Rails.application.config.spree.payment_methods << Spree::Gateway::FirstdataE4
+
+{
+  s3_credentials: {
+    access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+    secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+    bucket: ENV['S3_BUCKET_NAME']
+  },
+  storage: :s3,
+  s3_headers: { 'Cache-Control' => 'max-age=31557600' },
+  s3_protocol: 'https',
+  bucket: ENV['S3_BUCKET_NAME'],
+  url: ':s3_domain_url',
+  styles: {
+    mini: '48x48>',
+    small: '100x100>',
+    product: '240x240>',
+    large: '600x600>'
+  },
+  path: '/:class/:id/:style/:basename.:extension',
+  default_url: '/:class/:id/:style/:basename.:extension',
+  default_style: 'product'
+}.each do |key, value|
+  Spree::Image.attachment_definitions[:attachment][key.to_sym] = value
+end

0d9ff9afb4796b93c1e3843a97d6dbe09de21ca5
Spencer Applegate <spencer@spencerapplegate.com>
2015-05-16 20:55:44 -0400

    Fixed path to header partial

--- app/overrides/spree/shared/_header.rb
+++
@@ -1,6 +0,0 @@
-Deface::Override.new(
-  virtual_path: 'spree/shared/header',
-  name:         'header',
-  remove:       'div#spree-header',
-  partial:      'spree/shared/header'
-)
---
+++ app/overrides/spree/shared/header.rb
@@ -0,0 +1,6 @@
+Deface::Override.new(
+  virtual_path: 'spree/shared/_header',
+  name:         'header',
+  replace:       'div#spree-header',
+  partial:      'spree/shared/px_header'
+)

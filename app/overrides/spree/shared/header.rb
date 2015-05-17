713fa4dea8c62aac9ec35852e4c4ed2a3c4004b9
Spencer Applegate <spencer@spencerapplegate.com>
2015-05-16 21:52:16 -0400

    Added header with login

--- app/overrides/spree/shared/header.rb
+++ app/overrides/spree/shared/header.rb
@@ -1,6 +1,6 @@
 Deface::Override.new(
   virtual_path: 'spree/shared/_header',
   name:         'header',
-  replace:       'div#spree-header',
+  replace:      'div#spree-header',
   partial:      'spree/shared/px_header'
 )

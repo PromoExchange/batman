514706da86821395ef26e7f840d9eba931ff9fdd
Spencer Applegate <spencer@spencerapplegate.com>
2015-05-17 22:15:49 -0400

    Added about us link to nav bar

--- app/overrides/spree/shared/nav_bar.rb
+++ app/overrides/spree/shared/nav_bar.rb
@@ -6,6 +6,14 @@ Deface::Override.new(
   partial:       'spree/shared/login_bar'
 )
 
+# Add About Us page link
+Deface::Override.new(
+  virtual_path: 'spree/shared/_nav_bar',
+  name:         'nav_bar_insert_about_us',
+  insert_after: '#search-bar[data-hook]',
+  text:         '<li><a href="/aboutus">About Us</a></li>'
+)
+
 # Remove search
 Deface::Override.new(
   virtual_path: 'spree/shared/_nav_bar',

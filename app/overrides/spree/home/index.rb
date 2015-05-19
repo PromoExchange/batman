303d83d2c4815343a6f4f73c1f229ecc06a26b14
Spencer Applegate <spencer@spencerapplegate.com>
2015-05-18 22:38:48 -0400

    Added tab panel for buyer and seller info

--- app/overrides/spree/home/index.rb
+++ app/overrides/spree/home/index.rb
@@ -6,6 +6,14 @@ Deface::Override.new(
   partial:       'spree/home/carousel'
 )
 
+# Add buyer/seller info
+Deface::Override.new(
+  virtual_path:  'spree/home/index',
+  name:          'home_index_add_user_info',
+  insert_before: "[data-hook='homepage_sidebar_navigation']",
+  partial:       'spree/home/user_info'
+)
+
 # Remove products sidebar from homepage
 Deface::Override.new(
   virtual_path: 'spree/home/index',

d268a088f901ebfc2ff45fede38f293dfe0c0a41
tvarley <tim.varley@gmail.com>
2015-05-21 18:26:12 -0400

    Replace price with auction button

---
+++ app/overrides/spree/product/search_result_price.rb
@@ -0,0 +1,6 @@
+Deface::Override.new(
+  virtual_path:  'spree/shared/_products',
+  name:          'search_result_price',
+  replace_contents: 'div.panel-footer',
+  partial:       'spree/product/index_auction'
+)

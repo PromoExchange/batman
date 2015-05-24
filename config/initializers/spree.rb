2a7439137cf3b089a29c3369069739b0e4d1c675
Spencer Applegate <spencer@spencerapplegate.com>
2015-05-24 19:48:28 -0400

    Loaded dotenv file before env configs

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -24,12 +24,12 @@ Rails.application.config.spree.payment_methods << Spree::Gateway::FirstdataE4
   s3_credentials: {
     access_key_id: ENV['AWS_ACCESS_KEY_ID'],
     secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-    bucket: ENV['S3_BUCKET_NAME']
+    bucket: ENV['S3_IMAGE_BUCKET']
   },
   storage: :s3,
   s3_headers: { 'Cache-Control' => 'max-age=31557600' },
   s3_protocol: 'https',
-  bucket: ENV['S3_BUCKET_NAME'],
+  bucket: ENV['S3_IMAGE_BUCKET'],
   url: ':s3_domain_url',
   styles: {
     mini: '48x48>',

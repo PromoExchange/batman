e32a73c403fb650d1bb511a63e496439b4724df5
Spencer Applegate <spencer@spencerapplegate.com>
2015-06-01 21:39:33 -0400

    Added leeds adapter

--- db/products/leeds.rb
+++ db/products/leeds.rb
@@ -35,43 +35,43 @@ default_attrs = {
   available_on: Time.zone.now
 }
 
-file_name = File.join(Rails.root, 'db/product_data/high_caliber.csv')
+file_name = File.join(Rails.root, 'db/product_data/leeds.csv')
 CSV.foreach(file_name, headers: true, header_converters: :symbol) do |row|
   hashed = row.to_hash
 
   # Skip conditionals
-  next unless hashed[:qty_1] && hashed[:price_1] && hashed[:item_status] == 'Active/All Products'
+  next unless hashed[:priceqtycol1] && hashed[:priceuscol1]
 
   product_attrs = {
-    sku: hashed[:hcl_item],
-    name: hashed[:item_name],
-    description: hashed[:description],
+    sku: hashed[:itemno],
+    name: hashed[:itemname],
+    description: hashed[:catalogdescription],
     price: 0
   }
 
   begin
     product = Spree::Product.create!(default_attrs.merge(product_attrs))
   rescue => e
-    ap "Error in #{hashed[:hcl_item]} product data: #{e}"
+    ap "Error in #{hashed[:itemno]} product data: #{e}"
     next
   end
 
   # Prices
-  if hashed[:price_5]
+  if hashed[:priceuscol5]
     price_quantity = 5
-  elsif hashed[:price_4]
+  elsif hashed[:priceuscol4]
     price_quantity = 4
-  elsif hashed[:price_3]
+  elsif hashed[:priceuscol3]
     price_quantity = 3
-  elsif hashed[:price_2]
+  elsif hashed[:priceuscol2]
     price_quantity = 2
-  elsif hashed[:price_1]
+  elsif hashed[:priceuscol1]
     price_quantity = 1
   end
   (1..price_quantity).each do |i|
-    quantity_key1 = "qty_#{i}".to_sym
-    quantity_key2 = "qty_#{i + 1}".to_sym
-    price_key = "price_#{i}".to_sym
+    quantity_key1 = "priceqtycol#{i}".to_sym
+    quantity_key2 = "priceqtycol#{i + 1}".to_sym
+    price_key = "priceuscol#{i}".to_sym
 
     if i == price_quantity
       name = "#{hashed[quantity_key1]}+"
@@ -91,14 +91,15 @@ CSV.foreach(file_name, headers: true, header_converters: :symbol) do |row|
         discount_type: 'price'
       )
     rescue => e
-      ap "Error in #{hashed[:hcl_item]} pricing data: #{e}"
+      binding.pry
+      ap "Error in #{hashed[:itemno]} pricing data: #{e}"
     end
   end
 
   # Properties
   properties = []
-  properties << "Size:#{hashed[:item_size]}" if hashed[:item_size]
-  properties << "Status:#{hashed[:item_status]}" if hashed[:item_status]
+  properties << "Material:#{hashed[:material]}" if hashed[:material]
+  properties << "Size:#{hashed[:catalogsize]}" if hashed[:catalogsize]
 
   properties.each do |property|
     property_vals = property.split(':')

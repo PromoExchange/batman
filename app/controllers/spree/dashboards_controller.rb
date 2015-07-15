6cd8563b75b022703fdadd6fe2e287a4cdf40dac
tvarley <tim.varley@gmail.com>
2015-07-14 23:11:47 -0400

    Protect dashboards - must be signed in

--- app/controllers/spree/dashboards_controller.rb
+++ app/controllers/spree/dashboards_controller.rb
@@ -5,10 +5,17 @@
 # The tabs *should* (eventually) make API calls to get it's own data.
 # There is a risk that this will become heavy quickly.
 class Spree::DashboardsController < Spree::StoreController
+  before_action :require_login
   def index
     @favorites = Spree::Favorite.where(buyer: current_spree_user)
       .includes(:product)
 
     @user = spree_current_user
+  end
+
+  private
+
+  def require_login
+    redirect_to login_url unless current_spree_user
   end
 end
--- app/controllers/spree/user_controller_decorator.rb
+++ app/controllers/spree/user_controller_decorator.rb
@@ -1,10 +1,9 @@
-# @@user_attributes = [:email, :password, :password_confirmation]
 Spree::UsersController.class_eval do
   def update
     if @user.update_attributes(user_params)
       if params[:user][:password].present?
         user = Spree::User.reset_password_by_token(params[:user])
-        sign_in(@user, :event => :authentication, :bypass => !Spree::Auth::Config[:signout_after_password_change])
+        sign_in(user, event: :authentication, bypass: !Spree::Auth::Config[:signout_after_password_change])
       end
       redirect_to main_app.dashboards_url, notice: Spree.t(:account_updated)
     else

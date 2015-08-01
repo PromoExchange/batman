d7af2bbbf2b90a7374beab9435fa377cfdf07e57
tvarley <tim.varley@gmail.com>
2015-08-01 14:15:39 -0400

    Cosmetics
    - Links fixed
    - About link in header fixed
    - Registration link in footer fixed
    - Market place in footer was broken
    - Main nav should not wrap
    - Remove paypal tab
    - Shrink carousel bottle image
    - Add email to support page
    - Restore home link on logo
    - Adjust header
    - How it works buyer styled
    - Seller how it works
    - Fix FAQ pages
    - Front page styling

--- app/overrides/spree/layouts/spree_application.rb
+++ app/overrides/spree/layouts/spree_application.rb
@@ -5,3 +5,10 @@ Deface::Override.new(
   insert_after: 'div.container',
   partial:      'spree/shared/footer'
 )
+
+Deface::Override.new(
+  virtual_path: 'spree/layouts/spree_application',
+  name:         'google_fonts',
+  insert_bottom: '[data-hook="inside_head"]',
+  text: "<link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>"
+)
--- app/overrides/spree/shared/header.rb
+++
@@ -1,6 +0,0 @@
-# remove spree logo
-Deface::Override.new(
-  virtual_path: 'spree/shared/_header',
-  name:         'header_nav_bar_remove_logo',
-  remove:       'figure#logo'
-)

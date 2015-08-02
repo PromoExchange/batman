6191b1457b988e5f39a3b2ea5cbc4e55f3874b32
tvarley <tim.varley@gmail.com>
2015-08-02 17:19:07 -0400

    Add Mailtrap configuration

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -79,4 +79,14 @@ Rails.application.configure do
 
   # Seed data, load image files
   config.x.load_images = true
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :user_name => '40922294039e2b49c',
+    :password => 'f1714f931e428c',
+    :address => 'mailtrap.io',
+    :domain => 'mailtrap.io',
+    :port => '2525',
+    :authentication => :cram_md5
+  }
 end

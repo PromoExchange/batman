3bcadcb2157bb0b73d713d3320d657ad786e2d24
tvarley <tim.varley@gmail.com>
2015-09-21 12:07:47 -0400

    Cancel all current bids

--- app/models/spree/user_decorator.rb
+++ app/models/spree/user_decorator.rb
@@ -4,11 +4,15 @@ Spree::User.class_eval do
   has_many :addresses
 
   def ban
-    Spree::Bid.destroy_all(seller_id: id, status: :open)
+    cancel_bids
     update_attribute(:banned, true)
   end
 
   def unban
     update_attribute(:banned, false)
+  end
+
+  def cancel_bids
+    Spree::Bid.where(seller_id: id, state: 'open').map(&:cancel)
   end
 end
--- spec/models/spree/user_spec.rb
+++ spec/models/spree/user_spec.rb
@@ -5,4 +5,14 @@ RSpec.describe Spree::User, type: :model do
     t = Spree::User.reflect_on_association(:tax_rates)
     expect(t.macro).to eq :has_many
   end
+
+  it 'should cancel bids' do
+    b = FactoryGirl.create(:bid)
+    u = b.seller
+    count = Spree::Bid.where(seller: b.seller, state: 'open').count
+    expect(count).to eq 1
+    u.cancel_bids
+    count = Spree::Bid.where(seller: b.seller, state: 'open').count
+    expect(count).to eq 0
+  end
 end

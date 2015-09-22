71962518d66aa2815dd653d1f9257df15f85fc33
tvarley <tim.varley@gmail.com>
2015-09-22 13:33:18 -0400

    Fix multiple loads from dashboard

--- app/controllers/spree/logos_controller.rb
+++ app/controllers/spree/logos_controller.rb
@@ -1,12 +1,11 @@
 class Spree::LogosController < Spree::StoreController
   def create
     session[:return_to] ||= request.referer
-    session[:return_to] = session[:return_to]+'?tab=logo' if session[:return_to].include?('dashboards')
-    
+    session[:return_to] = main_app.dashboards_path(tab: 'logo') if session[:return_to].include?('dashboards')
     @logo = Spree::Logo.new(logo_params)
 
     if @logo.save
-      redirect_to session.delete(:return_to), flash: { notice: 'Logo upload sucessfully.' }  
+      redirect_to session.delete(:return_to), flash: { notice: 'Logo uploaded sucessfully.' }
     else
       redirect_to main_app.dashboards_path(tab: 'logo'), flash: { error: 'Failed to upload logo.' }
     end

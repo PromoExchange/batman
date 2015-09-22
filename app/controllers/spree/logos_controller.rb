a6282d973bfc364ce442bc3e6758d466b66413ad
Dipak <dipakgpta29@gmail.com>
2015-09-22 18:31:16 +0530

    Navigation after uploading logo to auction should stay on auction

--- app/controllers/spree/logos_controller.rb
+++ app/controllers/spree/logos_controller.rb
@@ -1,10 +1,12 @@
 class Spree::LogosController < Spree::StoreController
   def create
     session[:return_to] ||= request.referer
+    session[:return_to] = session[:return_to]+'?tab=logo' if session[:return_to].include?('dashboards')
+    
     @logo = Spree::Logo.new(logo_params)
 
     if @logo.save
-      redirect_to main_app.dashboards_path(tab: 'logo'), flash: { notice: 'Logo upload sucessfully.' }
+      redirect_to session.delete(:return_to), flash: { notice: 'Logo upload sucessfully.' }  
     else
       redirect_to main_app.dashboards_path(tab: 'logo'), flash: { error: 'Failed to upload logo.' }
     end

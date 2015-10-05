f4c256c9507710e76bf9f5fe214d8f1c30ce6cf7
tvarley <tim.varley@gmail.com>
2015-10-05 15:35:49 -0400

    Make seconds delay configurable

--- lib/email_helpers.rb
+++ lib/email_helpers.rb
@@ -1,6 +1,7 @@
 module EmailHelpers
   def self.email_delay(delay)
-    return delay unless (ENV['EMAIL_DELAY_TESTMODE'] == '1')
-    Time.zone.now + 10.seconds
+    return delay if ENV['EMAIL_DELAY_TESTMODE'].nil?
+    seconds_to_delay = ENV['EMAIL_DELAY_TESTMODE'].to_i
+    Time.zone.now + seconds_to_delay.seconds
   end
 end

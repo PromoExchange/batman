5c867bc6a2f09a6ab0bee400a49446b8fe298252
tvarley <tim.varley@gmail.com>
2015-10-07 19:26:15 -0400

    Fix specs

--- spec/factories/auction.rb
+++ spec/factories/auction.rb
@@ -12,6 +12,7 @@ FactoryGirl.define do
     payment_method 'Credit Card'
     state 'open'
     reference SecureRandom.hex(3)
+    proof_file { File.new(Rails.root.join('spec', 'fixtures', 'batman.jpeg')) }
   end
 
   factory :auction_with_bids, parent: :auction do

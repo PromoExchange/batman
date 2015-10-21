69fa74e98926b199153bada30af7a2d77be5caef
tvarley <tim.varley@gmail.com>
2015-10-21 14:22:47 -0400

    Remove request product

--- app/overrides/spree/product/request_product.rb
+++
@@ -1,6 +0,0 @@
-Deface::Override.new(
-  virtual_path:  'spree/shared/_products',
-  name:          'request_product',
-  insert_before: "erb[silent]:contains('classes')",
-  partial:       'spree/product/request_product'
-)

39e6dbef676c5b68e5ce82fb4100f5b7d57a1d01
tvarley <tim.varley@gmail.com>
2015-10-21 14:00:25 -0400

    Change text on product ideation flash message

--- app/controllers/spree/product_requests_controller.rb
+++ app/controllers/spree/product_requests_controller.rb
@@ -2,7 +2,7 @@ class Spree::ProductRequestsController < Spree::StoreController
   def create
     @product_request = Spree::ProductRequest.new(product_request_params)
     if @product_request.save
-      redirect_to '/dashboards', flash: { notice: 'Your PromoExchange rep will have product ideas for you soon!.' }
+      redirect_to '/dashboards', flash: { notice: 'Your PromoExchange swag pro will have product ideas for you soon!' }
     else
       redirect_to :back, flash: { error: @product_request.errors.full_messages.first }
     end

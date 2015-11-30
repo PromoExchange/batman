9e490082ed2acd409d4a9c7321d8e3ac83077cae
Dipak <dipakgpta29@gmail.com>
2015-11-30 11:43:17 +0530

    fixed art file logo pixelated issue

--- app/models/spree/logo.rb
+++ app/models/spree/logo.rb
@@ -2,13 +2,8 @@ class Spree::Logo < Spree::Base
   belongs_to :user
   validates :user_id, presence: true
 
-  has_attached_file :logo_file,
-    styles: {
-      thumb: '100x100>',
-      medium: '300x300>'
-    },
-    path: '/logo_file/:id/:style/:basename.:extension',
-    default_style: 'thumb'
+  has_attached_file :logo_file,path: '/logo_file/:id/:style/:basename.:extension'
+   
 
   validates_attachment_content_type :logo_file,
     content_type: %w(application/illustrator application/pdf image/x-eps image/vnd.adobe.photoshop)
@@ -18,7 +13,9 @@ class Spree::Logo < Spree::Base
   LOGO_FILE_NAME = { "pdf": "artwork/pdf.png", "ai": "artwork/ai.png", "eps": "artwork/eps.jpg",  "psd": "artwork/psd.jpg"}
 
   def artwork
-    extension = logo_file_file_name.split(".")[1].to_sym
-    "/assets/#{Spree::Logo::LOGO_FILE_NAME[extension]}"
+    if logo_file_file_name.present?
+      extension = logo_file_file_name.split(".")[1].to_sym
+      "/assets/#{Spree::Logo::LOGO_FILE_NAME[extension]}"
+    end  
   end
 end

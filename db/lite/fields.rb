c7847bf8cdc9d632d1c239dceee629c3d239fa4c
tvarley <tim.varley@gmail.com>
2015-12-18 13:51:18 -0500

    Fields load and fixes to Gemline Load

--- db/lite/fields.rb
+++ db/lite/fields.rb
@@ -1,182 +1,92 @@
 require 'csv'
 require 'open-uri'
 
-puts 'Loading Fields products'
+def add_charge(product, imprint_method, upcharge_type, value, range, price_code, position)
+  upcharge = Spree::UpchargeProduct.where(
+    product: product,
+    imprint_method: imprint_method,
+    upcharge_type: upcharge_type
+  ).first_or_create
+  upcharge.update_attributes(
+    value: value,
+    range: range,
+    price_code: price_code,
+    position: position
+  )
+end
 
-supplier = Spree::Supplier.where(name: 'Fields').first_or_create
-
-file_name = File.join(Rails.root, 'db/product_data/fields.csv')
-load_fail = 0
-count = 0
-beginning_time = Time.zone.now
-
-CSV.foreach(file_name, headers: true, header_converters: :symbol) do |row|
-  hashed = row.to_hash
-
-  count += 1
-
-  product = Spree::Product.joins(:master).where("spree_variants.sku='#{hashed[:item_]}'").first
-
-  if product.nil?
-    puts "ERROR: Failed to find product [#{hashed[:item_]}]"
-    next
-  end
-
-  product.carton.weight = hashed[:weight_per_carton_lbs]
-  product.carton.quantity = hashed[:quantity_per_box]
-  product.carton.originating_zip = '01843-1066'
-  dimensions = hashed[:carton_dimensions].gsub(/[A-Z]/, '').delete(' ').split('x')
-  product.carton.length = dimensions[0]
-  product.carton.width = dimensions[1]
-  product.carton.height = dimensions[2]
-
-  product.carton.save!
-
+def add_upcharges(product)
   # upcharges
   setup_upcharge = Spree::UpchargeType.where(name: 'setup').first
   run_upcharge = Spree::UpchargeType.where(name: 'run').first
 
   screen_print_imprint = Spree::ImprintMethod.where(name: 'Screen Print').first_or_create
-  deboss_imprint = Spree::ImprintMethod.where(name: 'Deboss').first_or_create
-  logomatic_imprint = Spree::ImprintMethod.where(name: 'Logomatic').first_or_create
-  embroidery_imprint = Spree::ImprintMethod.where(name: 'Embroidery').first_or_create
-  gemphoto_imprint = Spree::ImprintMethod.where(name: 'Gemphoto').first_or_create
 
-  product.upcharges.destroy_all
-
-  Spree::UpchargeProduct.where(
-    product: product,
-    imprint_method: screen_print_imprint,
-    value: '55',
-    price_code: 'V',
-    upcharge_type: setup_upcharge
-  ).first_or_create
-
-  Spree::UpchargeProduct.where(
-    product: product,
-    imprint_method: screen_print_imprint,
-    value: '0.99',
-    upcharge_type: run_upcharge,
-    price_code: 'V',
-    range: '(6..99)',
-    position: 1
-  ).first_or_create
-
-  Spree::UpchargeProduct.where(
-    product: product,
-    imprint_method: screen_print_imprint,
-    value: '0.74',
-    upcharge_type: run_upcharge,
-    price_code: 'V',
-    range: '(100..299)',
-    position: 2
-  ).first_or_create
-
-  Spree::UpchargeProduct.where(
-    product: product,
-    imprint_method: screen_print_imprint,
-    value: '0.59',
-    upcharge_type: run_upcharge,
-    price_code: 'V',
-    range: '(300..999)',
-    position: 3
-  ).first_or_create
-
-  Spree::UpchargeProduct.where(
-    product: product,
-    imprint_method: screen_print_imprint,
-    value: '0.45',
-    upcharge_type: run_upcharge,
-    price_code: 'V',
-    range: '1000+',
-    position: 4
-  ).first_or_create
-
-  Spree::UpchargeProduct.where(
-    product: product,
-    imprint_method: embroidery_imprint,
-    value: '2.80',
-    upcharge_type: run_upcharge,
-    price_code: 'V',
-    range: '(6..99)',
-    position: 1
-  ).first_or_create
-
-  Spree::UpchargeProduct.where(
-    product: product,
-    imprint_method: embroidery_imprint,
-    value: '2.55',
-    upcharge_type: run_upcharge,
-    price_code: 'V',
-    range: '(100..299)',
-    position: 2
-  ).first_or_create
-
-  Spree::UpchargeProduct.where(
-    product: product,
-    imprint_method: embroidery_imprint,
-    value: '2.29',
-    price_code: 'V',
-    upcharge_type: run_upcharge,
-    range: '300+',
-    position: 3
-  ).first_or_create
-
-  Spree::UpchargeProduct.where(
-    product: product,
-    imprint_method: deboss_imprint,
-    value: '70',
-    price_code: 'V',
-    upcharge_type: setup_upcharge
-  ).first_or_create
-
-  Spree::UpchargeProduct.where(
-    product: product,
-    imprint_method: logomatic_imprint,
-    value: '55',
-    price_code: 'V',
-    upcharge_type: setup_upcharge
-  ).first_or_create
-
-  Spree::UpchargeProduct.where(
-    product: product,
-    imprint_method: gemphoto_imprint,
-    value: '2.80',
-    upcharge_type: run_upcharge,
-    price_code: 'V',
-    range: '(6..99)',
-    position: 1
-  ).first_or_create
-
-  Spree::UpchargeProduct.where(
-    product: product,
-    imprint_method: gemphoto_imprint,
-    value: '2.55',
-    upcharge_type: run_upcharge,
-    price_code: 'V',
-    range: '(100..299)',
-    position: 2
-  ).first_or_create
-
-  Spree::UpchargeProduct.where(
-    product: product,
-    imprint_method: gemphoto_imprint,
-    value: '2.20',
-    upcharge_type: run_upcharge,
-    price_code: 'V',
-    range: '(300..999)',
-    position: 3
-  ).first_or_create
-
-  Spree::UpchargeProduct.where(
-    product: product,
-    imprint_method: gemphoto_imprint,
-    value: '2.05',
-    upcharge_type: run_upcharge,
-    price_code: 'V',
-    range: '1000+',
-    position: 4
-  ).first_or_create
+  add_charge(product, screen_print_imprint, setup_upcharge, '50', '', 'G',0)
+  add_charge(product, screen_print_imprint, run_upcharge, '0.4', '1+', 'G',0)
 end
 
-puts "Products updated: #{count}"
+puts 'Loading Fields products'
+
+supplier = Spree::Supplier.where(dc_acct_num: '100156')
+
+file_name = File.join(Rails.root, 'db/product_data/fields.csv')
+db_load_fail = 0
+db_product_count = Spree::Product.where(supplier: supplier).count
+in_file_count = 0
+found_ids = []
+invalid_carton_count = 0
+updated_carton_count = 0
+updated_upcharge_count = 0
+
+CSV.foreach(file_name, headers: true, header_converters: :symbol) do |row|
+  hashed = row.to_hash
+
+  in_file_count += 1
+
+  product = Spree::Product.joins(:master)
+    .where(supplier: supplier)
+    .where("spree_variants.sku='#{hashed[:productcode]}'").first
+
+  if product.nil?
+    db_load_fail += 1
+    puts "ERROR: Failed to find product [#{hashed[:productcode]}]"
+    next
+  end
+
+  found_ids << product.id
+
+  product.carton.weight = hashed[:carton_weight_lbs]
+  product.carton.quantity = hashed[:units_per_carton]
+  product.carton.originating_zip = hashed[:fob].delete(' ').split('|')[0]
+  product.carton.length = hashed[:carton_length]
+  product.carton.width = hashed[:carton_width]
+  product.carton.height = hashed[:carton_height]
+
+  if product.carton.active?
+    product.carton.save!
+    updated_carton_count += 1
+  else
+    invalid_carton_count += 1
+  end
+
+  add_upcharges(product)
+  updated_upcharge_count += 1
+end
+
+# Add upcharges to those remaining
+in_db_only = Spree::Product.where(supplier: supplier).where.not(id: found_ids).count
+
+Spree::Product.where(supplier: supplier).where.not(id: found_ids).each do |prod|
+  add_upcharges(prod)
+  updated_upcharge_count += 1
+end
+
+puts "Products in XML: #{in_file_count}"
+puts "Products in DB: #{db_product_count}"
+puts "Products in XML AND DB: #{found_ids.count}"
+puts "Products in XML only: #{db_load_fail}"
+puts "Products in DB only: #{in_db_only}"
+puts "Products with invalid cartons: #{invalid_carton_count}"
+puts "Products updated with carton: #{updated_carton_count}"
+puts "Products updated with upcharges: #{updated_upcharge_count}"
--- db/lite/gemline.rb
+++ db/lite/gemline.rb
@@ -1,6 +1,20 @@
 require 'csv'
 require 'open-uri'
 
+def add_charge(product, imprint_method, upcharge_type, value, range, price_code, position)
+  upcharge = Spree::UpchargeProduct.where(
+    product: product,
+    imprint_method: imprint_method,
+    upcharge_type: upcharge_type
+  ).first_or_create
+  upcharge.update_attributes(
+    value: value,
+    range: range,
+    price_code: price_code,
+    position: position
+  )
+end
+
 def add_upcharges(product)
   # upcharges
   setup_upcharge = Spree::UpchargeType.where(name: 'setup').first
@@ -12,152 +26,36 @@ def add_upcharges(product)
   embroidery_imprint = Spree::ImprintMethod.where(name: 'Embroidery').first_or_create
   gemphoto_imprint = Spree::ImprintMethod.where(name: 'Gemphoto').first_or_create
 
-  product.upcharges.destroy_all
+  add_charge(product, screen_print_imprint, setup_upcharge, '55', '', 'V', 0)
+  add_charge(product, screen_print_imprint, run_upcharge, '0.99', '(6..99)', 'V', 1)
+  add_charge(product, screen_print_imprint, run_upcharge, '0.74', '(100..299)', 'V', 2)
+  add_charge(product, screen_print_imprint, run_upcharge, '0.59', '(300..999)', 'V', 2)
+  add_charge(product, screen_print_imprint, run_upcharge, '0.45', '1000+', 'V', 4)
 
-  Spree::UpchargeProduct.where(
-    product: product,
-    imprint_method: screen_print_imprint,
-    value: '55',
-    price_code: 'V',
-    upcharge_type: setup_upcharge
-  ).first_or_create
+  add_charge(product, embroidery_imprint, run_upcharge, '2.80', '(6..99)', 'V', 1)
+  add_charge(product, embroidery_imprint, run_upcharge, '2.55', '(100..299)', 'V', 2)
+  add_charge(product, embroidery_imprint, run_upcharge, '2.29', '300+', 'V', 3)
 
-  Spree::UpchargeProduct.where(
-    product: product,
-    imprint_method: screen_print_imprint,
-    value: '0.99',
-    upcharge_type: run_upcharge,
-    price_code: 'V',
-    range: '(6..99)',
-    position: 1
-  ).first_or_create
+  add_charge(product, deboss_imprint, setup_upcharge, '70', '', 'V', 0)
 
-  Spree::UpchargeProduct.where(
-    product: product,
-    imprint_method: screen_print_imprint,
-    value: '0.74',
-    upcharge_type: run_upcharge,
-    price_code: 'V',
-    range: '(100..299)',
-    position: 2
-  ).first_or_create
+  add_charge(product, logomatic_imprint, setup_upcharge, '55', '', 'V', 0)
 
-  Spree::UpchargeProduct.where(
-    product: product,
-    imprint_method: screen_print_imprint,
-    value: '0.59',
-    upcharge_type: run_upcharge,
-    price_code: 'V',
-    range: '(300..999)',
-    position: 3
-  ).first_or_create
-
-  Spree::UpchargeProduct.where(
-    product: product,
-    imprint_method: screen_print_imprint,
-    value: '0.45',
-    upcharge_type: run_upcharge,
-    price_code: 'V',
-    range: '1000+',
-    position: 4
-  ).first_or_create
-
-  Spree::UpchargeProduct.where(
-    product: product,
-    imprint_method: embroidery_imprint,
-    value: '2.80',
-    upcharge_type: run_upcharge,
-    price_code: 'V',
-    range: '(6..99)',
-    position: 1
-  ).first_or_create
-
-  Spree::UpchargeProduct.where(
-    product: product,
-    imprint_method: embroidery_imprint,
-    value: '2.55',
-    upcharge_type: run_upcharge,
-    price_code: 'V',
-    range: '(100..299)',
-    position: 2
-  ).first_or_create
-
-  Spree::UpchargeProduct.where(
-    product: product,
-    imprint_method: embroidery_imprint,
-    value: '2.29',
-    price_code: 'V',
-    upcharge_type: run_upcharge,
-    range: '300+',
-    position: 3
-  ).first_or_create
-
-  Spree::UpchargeProduct.where(
-    product: product,
-    imprint_method: deboss_imprint,
-    value: '70',
-    price_code: 'V',
-    upcharge_type: setup_upcharge
-  ).first_or_create
-
-  Spree::UpchargeProduct.where(
-    product: product,
-    imprint_method: logomatic_imprint,
-    value: '55',
-    price_code: 'V',
-    upcharge_type: setup_upcharge
-  ).first_or_create
-
-  Spree::UpchargeProduct.where(
-    product: product,
-    imprint_method: gemphoto_imprint,
-    value: '2.80',
-    upcharge_type: run_upcharge,
-    price_code: 'V',
-    range: '(6..99)',
-    position: 1
-  ).first_or_create
-
-  Spree::UpchargeProduct.where(
-    product: product,
-    imprint_method: gemphoto_imprint,
-    value: '2.55',
-    upcharge_type: run_upcharge,
-    price_code: 'V',
-    range: '(100..299)',
-    position: 2
-  ).first_or_create
-
-  Spree::UpchargeProduct.where(
-    product: product,
-    imprint_method: gemphoto_imprint,
-    value: '2.20',
-    upcharge_type: run_upcharge,
-    price_code: 'V',
-    range: '(300..999)',
-    position: 3
-  ).first_or_create
-
-  Spree::UpchargeProduct.where(
-    product: product,
-    imprint_method: gemphoto_imprint,
-    value: '2.05',
-    upcharge_type: run_upcharge,
-    price_code: 'V',
-    range: '1000+',
-    position: 4
-  ).first_or_create
+  add_charge(product, gemphoto_imprint, run_upcharge, '2.80', '(6..99)', 'V', 1)
+  add_charge(product, gemphoto_imprint, run_upcharge, '2.55', '(100..299)', 'V', 2)
+  add_charge(product, gemphoto_imprint, run_upcharge, '2.20', '(300..999)', 'V', 3)
+  add_charge(product, gemphoto_imprint, run_upcharge, '2.05', '1000+', 'V', 4)
 end
 
 puts 'Loading Gemline products'
 
-supplier = Spree::Supplier.where(name: 'Gemline').first_or_create
+supplier = Spree::Supplier.where(dc_acct_num: '100257')
 
 file_name = File.join(Rails.root, 'db/product_data/gemline.csv')
 db_load_fail = 0
 db_product_count = Spree::Product.where(supplier: supplier).count
 in_file_count = 0
 found_ids = []
+invalid_carton_count = 0
 updated_carton_count = 0
 updated_upcharge_count = 0
 
@@ -165,8 +63,7 @@ CSV.foreach(file_name, headers: true, header_converters: :symbol) do |row|
   hashed = row.to_hash
 
   in_file_count += 1
-
-  product = Spree::Product.joins(:master).where("spree_variants.sku='#{hashed[:item_]}'").first
+  product = Spree::Product.joins(:master).where(supplier: supplier).where("spree_variants.sku='#{hashed[:item_]}'").first
 
   if product.nil?
     db_load_fail += 1
@@ -183,8 +80,13 @@ CSV.foreach(file_name, headers: true, header_converters: :symbol) do |row|
   product.carton.length = dimensions[0]
   product.carton.width = dimensions[1]
   product.carton.height = dimensions[2]
-  product.carton.save!
-  updated_carton_count += 1
+
+  if product.carton.active?
+    product.carton.save!
+    updated_carton_count += 1
+  else
+    invalid_carton_count += 1
+  end
 
   add_upcharges(product)
   updated_upcharge_count += 1
@@ -203,5 +105,6 @@ puts "Products in DB: #{db_product_count}"
 puts "Products in XML AND DB: #{found_ids.count}"
 puts "Products in XML only: #{db_load_fail}"
 puts "Products in DB only: #{in_db_only}"
+puts "Products with invalid cartons: #{invalid_carton_count}"
 puts "Products updated with carton: #{updated_carton_count}"
 puts "Products updated with upcharges: #{updated_upcharge_count}"

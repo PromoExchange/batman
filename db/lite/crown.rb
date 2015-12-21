6998239ea0f8881a265ce684126862070b6af36a
tvarley <tim.varley@gmail.com>
2015-12-21 08:17:18 -0500

    Add Crown cartons and upcharges

--- db/lite/crown.rb
+++ db/lite/crown.rb
@@ -15,6 +15,26 @@ def add_charge(product, imprint_method, upcharge_type, value, range, price_code,
   )
 end
 
+def add_qty_upcharges(product, imprint_method, upcharge_type, hashed)
+  volume_price = Spree::VolumePrice.where(variant_id: product.master).order(:position)
+  quantity_count = 1
+  volume_price.each do |v|
+    value = hashed["run_chargeqty#{quantity_count}".to_sym]
+    range = v.range
+    position = quantity_count
+    add_charge(
+      product,
+      imprint_method,
+      upcharge_type,
+      value,
+      range,
+      hashed[:code],
+      position
+    )
+    quantity_count += 1
+  end
+end
+
 def add_upcharges(product)
   # upcharges
   setup_upcharge = Spree::UpchargeType.where(name: 'setup').first
@@ -102,7 +122,7 @@ CSV.foreach(file_name, headers: true, header_converters: :symbol) do |row|
     if colors.count > 0
       Spree::ColorProduct.where(product: product).destroy_all
       colors.each do |color|
-        Spree::ColorProduct.create( product: product , color: color)
+        Spree::ColorProduct.create( product: product , color: color.strip)
       end
       updated_main_color += 1
     end
@@ -113,6 +133,214 @@ CSV.foreach(file_name, headers: true, header_converters: :symbol) do |row|
 end
 
 in_db_only = Spree::Product.where(supplier: supplier).where.not(id: found_ids).count
+
+upcharge_db_load_fail = 0
+upcharge_found_ids = []
+upcharge_in_file_count = 0
+updated_upcharge_count = 0
+
+file_name = File.join(Rails.root, 'db/upcharge_data/crown_product.csv')
+CSV.foreach(file_name, headers: true, header_converters: :symbol) do |row|
+  hashed = row.to_hash
+  
+  if upcharge_in_file_count % 10
+    puts "."
+  end
+
+  sku = hashed[:item_sku]
+
+  upcharge_in_file_count += 1
+
+  product = Spree::Product.joins(:master).where(supplier: supplier).where("spree_variants.sku='#{sku}'").first
+
+  if product.nil?
+    upcharge_db_load_fail += 1
+    # puts "ERROR: Failed to find product [#{sku}]"
+    next
+  end
+
+  upcharge_found_ids << product.id
+
+  setup_upcharge = Spree::UpchargeType.where(name: 'setup').first
+  run_upcharge = Spree::UpchargeType.where(name: 'additional_color_run').first
+
+  Spree::UpchargeProduct.where(product: product).destroy_all
+
+  screen_print_imprint = Spree::ImprintMethod.where(name: 'Screen Print').first_or_create
+  pen_imprint = Spree::ImprintMethod.where(name: 'Pen').first_or_create
+  deboss_imprint = Spree::ImprintMethod.where(name: 'Deboss').first_or_create
+  laser_engraving_imprint = Spree::ImprintMethod.where(name: 'Laser Engraving').first_or_create
+  color_label_imprint = Spree::ImprintMethod.where(name: 'Color Label').first_or_create
+  four_color_imprint = Spree::ImprintMethod.where(name: 'Four Color Process').first_or_create
+  # logomatic_imprint = Spree::ImprintMethod.where(name: 'Logomatic').first_or_create
+  # embroidery_imprint = Spree::ImprintMethod.where(name: 'Embroidery').first_or_create
+  # gemphoto_imprint = Spree::ImprintMethod.where(name: 'Gemphoto').first_or_create
+
+  value = 0
+
+  charge_name = hashed[:charge_name].strip
+  if charge_name == 'Set-Up Charge/Change of Copy'
+    add_charge(
+      product,
+      screen_print_imprint,
+      setup_upcharge,
+      hashed[:setup],
+      '1+',
+      hashed[:code],
+      0
+    )
+    updated_upcharge_count += 1
+  elsif charge_name == 'Per Color Run Charge'
+    imprint_method = screen_print_imprint
+    upcharge_type = run_upcharge
+    add_qty_upcharges(product, imprint_method, upcharge_type, hashed)
+    updated_upcharge_count += 1
+  elsif charge_name == 'Debossing Set-Up/Change of Copy'
+    add_charge(
+      product,
+      deboss_imprint,
+      setup_upcharge,
+      hashed[:setup],
+      '1+',
+      hashed[:code],
+      0
+    )
+    updated_upcharge_count += 1
+  elsif charge_name == 'Pen Imprint Set-Up Charge'
+    add_charge(
+      product,
+      pen_imprint,
+      setup_upcharge,
+      hashed[:setup],
+      '1+',
+      hashed[:code],
+      0
+    )
+    updated_upcharge_count += 1
+  elsif charge_name == 'Pen Imprint Run Charge'
+    imprint_method = pen_imprint
+    upcharge_type = run_upcharge
+    add_qty_upcharges(product, imprint_method, upcharge_type, hashed)
+    updated_upcharge_count += 1
+  elsif charge_name == '2nd Location Imprint'
+    next
+  elsif charge_name == 'Sharpening'
+    next
+  elsif charge_name == '2nd Side Imprint'
+    next
+  elsif charge_name == '2nd Pole Imprint Run Charge'
+    next
+  elsif charge_name == '2nd Location Imprint'
+    next
+  elsif charge_name == 'Multi Color Label Imprint Run Charge'
+    imprint_method = color_label_imprint
+    upcharge_type = run_upcharge
+    add_qty_upcharges(product, imprint_method, upcharge_type, hashed)
+    updated_upcharge_count += 1
+  elsif charge_name == 'Optional Carabiner Charge'
+    next
+  elsif charge_name == 'Multi Color Run Charge  Per Color'
+    imprint_method = screen_print_imprint
+    upcharge_type = run_upcharge
+    add_qty_upcharges(product, imprint_method, upcharge_type, hashed)
+    updated_upcharge_count += 1
+  elsif charge_name == '2nd Location & Multi Color Imprint Charge'
+    imprint_method = screen_print_imprint
+    upcharge_type = run_upcharge
+    add_qty_upcharges(product, imprint_method, upcharge_type, hashed)
+    updated_upcharge_count += 1
+  elsif charge_name == 'Optional Pen Imprint Run Charge Per Piece'
+    next
+  elsif charge_name == 'Color Surge PMS Color Match Per Color'
+    next
+  elsif charge_name == 'Color Surge 4 Color Process Run Charge'
+    imprint_method = four_color_imprint
+    upcharge_type = run_upcharge
+    add_qty_upcharges(product, imprint_method, upcharge_type, hashed)
+    updated_upcharge_count += 1
+  elsif charge_name == 'Set-Up/Change of Copy/Reorder'
+    add_charge(
+      product,
+      screen_print_imprint,
+      setup_upcharge,
+      hashed[:setup],
+      '1+',
+      hashed[:code],
+      0
+    )
+    updated_upcharge_count += 1
+  elsif charge_name == 'Sleeve Imprint Run Charge'
+    next
+  elsif charge_name == 'Silk Screen Set-Up/Change of Copy'
+    add_charge(
+      product,
+      screen_print_imprint,
+      setup_upcharge,
+      hashed[:setup],
+      '1+',
+      hashed[:code],
+      0
+    )
+    updated_upcharge_count += 1
+  elsif charge_name == 'Laser Engraving Set-Up/Change of Copy'
+    add_charge(
+      product,
+      laser_engraving_imprint,
+      setup_upcharge,
+      hashed[:setup],
+      '1+',
+      hashed[:code],
+      0
+    )
+    updated_upcharge_count += 1
+  elsif charge_name == 'Assembly Charge'
+    next
+  elsif charge_name == 'Per Additional Color Imprint'
+    imprint_method = screen_print_imprint
+    upcharge_type = run_upcharge
+    add_qty_upcharges(product, imprint_method, upcharge_type, hashed)
+    updated_upcharge_count += 1
+  elsif charge_name == 'Color Surge Digital Color Run Charge'
+    imprint_method = four_color_imprint
+    upcharge_type = run_upcharge
+    add_qty_upcharges(product, imprint_method, upcharge_type, hashed)
+    updated_upcharge_count += 1
+  elsif charge_name == 'Optional Pen Imprint Run Charge Per Piece'
+    next
+  elsif charge_name == '2nd Location Engraving'
+    next
+  elsif charge_name == 'File Preload'
+    next
+  elsif charge_name == 'Multi Color Imprint Run Charge'
+    imprint_method = four_color_imprint
+    upcharge_type = run_upcharge
+    add_qty_upcharges(product, imprint_method, upcharge_type, hashed)
+    updated_upcharge_count += 1
+  elsif charge_name == 'Color Surge'
+    add_charge(
+      product,
+      four_color_imprint,
+      setup_upcharge,
+      hashed[:setup],
+      '1+',
+      hashed[:code],
+      0
+    )
+    updated_upcharge_count += 1
+  elsif charge_name == 'Color Surge Run Charge'
+    imprint_method = four_color_imprint
+    upcharge_type = run_upcharge
+    add_qty_upcharges(product, imprint_method, upcharge_type, hashed)
+    updated_upcharge_count += 1
+  elsif charge_name == '2nd Location Imprint Run Charge'
+    next
+  elsif charge_name == 'Optional Pen Imprint Set-Up/Change of Copy'
+    next
+  else
+    puts "**** #{charge_name}"
+  end
+end
+
 
 # Add upcharges to those remaining
 # Spree::Product.where(supplier: supplier).where.not(id: found_ids).each do |prod|
@@ -127,5 +355,7 @@ puts "Products in XML only: #{db_load_fail}"
 puts "Products in DB only: #{in_db_only}"
 puts "Products with invalid cartons: #{invalid_carton_count}"
 puts "Products updated with carton: #{updated_carton_count}"
-puts "Products updated with upcharges: #{updated_upcharge_count}"
+puts "Upcharge Products in DB: #{upcharge_found_ids.count}"
+puts "Upcharges found in file: #{upcharge_in_file_count}"
+puts "Upcharges added: #{updated_upcharge_count}"
 puts "Products updated with main product color: #{updated_main_color}"

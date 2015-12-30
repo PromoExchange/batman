66c2a9c5bf861408c15979c6b703c8e582d6ad34
tvarley <tim.varley@gmail.com>
2015-12-30 16:04:15 -0500

    Hotfix EPS upload

--- app/models/spree/logo.rb
+++ app/models/spree/logo.rb
@@ -2,11 +2,11 @@ class Spree::Logo < Spree::Base
   belongs_to :user
   validates :user_id, presence: true
 
-  has_attached_file :logo_file,path: '/logo_file/:id/:style/:basename.:extension'
-   
+  has_attached_file :logo_file, path: '/logo_file/:id/:style/:basename.:extension'
+
 
   validates_attachment_content_type :logo_file,
-    content_type: %w(application/illustrator application/pdf image/x-eps image/vnd.adobe.photoshop)
+    content_type: %w(application/illustrator application/pdf application/postscript image/vnd.adobe.photoshop)
 
   validates_attachment :logo_file, presence: true
 
@@ -16,6 +16,6 @@ class Spree::Logo < Spree::Base
     if logo_file_file_name.present?
       extension = logo_file_file_name.split(".").last.to_sym
       "/assets/#{Spree::Logo::LOGO_FILE_NAME[extension]}"
-    end  
+    end
   end
 end

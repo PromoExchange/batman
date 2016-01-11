8d3097cfdd558b3d9fd831bb2f60ab5e95126601
tvarley <tim.varley@gmail.com>
2016-01-11 16:56:15 -0500

    Anchorfree Layout

--- app/controllers/spree/company_store_controller.rb
+++ app/controllers/spree/company_store_controller.rb
@@ -8,6 +8,7 @@ class Spree::CompanyStoreController < Spree::StoreController
 
   def fetch_company_store
     @company_store = Spree::CompanyStore.where(slug: params[:id]).first
+    @products = Spree::Product.where(supplier: @company_store.supplier)
   end
 
   def company_store_params

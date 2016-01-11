b161e4ccc7f4343d91ab140f9dbe51f88bdefada
tvarley <tim.varley@gmail.com>
2016-01-11 16:17:06 -0500

    Load anchorfree images and data

--- db/company_store/anchorfree.rb
+++ db/company_store/anchorfree.rb
@@ -10,6 +10,8 @@ supplier = Spree::Supplier.where(name: store_name).first
 
 fail 'Unable to find supplier' if supplier.nil?
 
+Spree::Product.where(supplier: supplier).destroy_all
+
 shipping_category = Spree::ShippingCategory.find_by_name!('Default')
 tax_category = Spree::TaxCategory.find_by_name!('Default')
 
@@ -20,3 +22,49 @@ default_attrs = {
 }
 
 file_name = File.join(Rails.root, 'db/company_store_data/anchorfree.csv')
+
+load_fail = 0
+image_fail = 0
+count = 0
+beginning_time = Time.zone.now
+
+CSV.foreach(file_name, headers: true, header_converters: :symbol) do |row|
+  hashed = row.to_hash
+
+  count += 1
+  begin
+    desc = hashed[:product_description]
+
+    product_attrs = {
+      sku: hashed[:sku],
+      name: hashed[:item_name],
+      description: hashed[:product_description],
+      price: 1.0,
+      supplier_id: supplier.id
+    }
+
+    product = Spree::Product.create!(default_attrs.merge(product_attrs))
+
+    # Image
+    if Rails.configuration.x.load_images
+      begin
+        image_path = File.join(Rails.root, "db/product_images/anchorfree/#{product_attrs[:sku]}.jpg")
+        product.images << Spree::Image.create!(
+          attachment: open(image_path),
+          viewable: product)
+      rescue => e
+        ap "Warning: Unable to load product image [#{product_attrs[:sku]}], #{e}"
+        image_fail += 1
+      end
+    end
+
+    # Main Product Color
+    Spree::ColorProduct.where(product: product, color: hashed[:color]).first_or_create
+
+  rescue => e
+    load_fail += 1
+    ap "Error in #{hashed[:sku]} product data: #{e}"
+  ensure
+    ActiveRecord::Base.clear_active_connections!
+  end
+end

9f87bc7b5f46fe84da018450c300e994f713bf1c
tvarley <tim.varley@gmail.com>
2016-01-18 13:54:15 -0500

    Fix lower case price code

--- spec/models/spree/price_spec.rb
+++ spec/models/spree/price_spec.rb
@@ -45,4 +45,17 @@ RSpec.describe Spree::Price, type: :model do
     t = Spree::Price.discount_price('5P', value)
     expect(((value * discount) - t).abs).to be < 0.0001
   end
+
+  it 'should discount price with lower case code' do
+    discount = 0.50
+    value = 1200
+    t = Spree::Price.discount_price('p', value)
+    expect(((value * discount) - t).abs).to be < 0.0001
+  end
+
+  it 'should (not) discount price with nil' do
+    value = 1200
+    t = Spree::Price.discount_price(nil, value)
+    expect(((value) - t).abs).to be < 0.0001
+  end
 end

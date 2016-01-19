ec6ebf43f82bef948eb75b2c23ab4b1819d4f0dd
tvarley <tim.varley@gmail.com>
2016-01-19 15:56:02 -0500

    Display lowest price

--- app/controllers/spree/company_store_controller.rb
+++ app/controllers/spree/company_store_controller.rb
@@ -6,7 +6,8 @@ class Spree::CompanyStoreController < Spree::StoreController
 
   def fetch_company_store
     @company_store = Spree::CompanyStore.where(slug: params[:id]).first
-    @products = Spree::Product.where(supplier: @company_store.supplier).order(:id)
+    products = Spree::Product.where(supplier: @company_store.supplier).pluck(:id)
+    @auctions = Spree::Auction.where(product_id: products, state: :custom_auction)
   end
 
   def company_store_params

deca5b35d7a67b73dacf190575b5a802433ad64d
tvarley <tim.varley@gmail.com>
2016-01-22 16:43:47 -0500

    Still use param id in company store controller

--- app/controllers/spree/company_store_controller.rb
+++ app/controllers/spree/company_store_controller.rb
@@ -10,8 +10,7 @@ class Spree::CompanyStoreController < Spree::StoreController
   private
 
   def fetch_company_store
-    company_store_id = ENV['COMPANYSTORE_ID']
-    @company_store = Spree::CompanyStore.where(slug: company_store_id).first
+    @company_store = Spree::CompanyStore.where(slug: params[:id]).first
     products = Spree::Product.where(supplier: @company_store.supplier).pluck(:id)
     @auctions = Spree::Auction.where(product_id: products, state: :custom_auction)
   end

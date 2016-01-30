8edd870b1dcd80afed3409d755df735209de8f23
tvarley <tim.varley@gmail.com>
2016-01-30 07:54:50 -0500

    Delay keyup event to prevent run in

--- vendor/assets/javascripts/spree/frontend/company_store/new.js
+++ vendor/assets/javascripts/spree/frontend/company_store/new.js
@@ -1,27 +1,37 @@
 $(function(){
+  var delay = (function(){
+    var timer = 0;
+    return function(callback, ms){
+      clearTimeout (timer);
+      timer = setTimeout(callback, ms);
+      };
+  })();
+
   $(".cs-quantity").keyup(function() {
-    var e = $(this);
-    var min = parseInt(e.attr('min'));
-    var actual = parseInt(e.val());
-    if (actual >= min) {
-      var auction_clone_id = $("#auction_clone_id").val();
-      var api_key = $('#new-auction').attr('data-key');
-      var url = '/api/auctions/'+auction_clone_id+'/best_price?quantity='+actual;
-      $.ajax({
-        type: 'GET',
-        contentType: "application/json",
-        url: url,
-        headers: {
-          'X-Spree-Token': api_key
-        },
-        success: function(data) {
-          var money_text = accounting.formatMoney((parseFloat(data)));
-          $(".cs-active-price").text(money_text);
-        },
-        error: function(data) {
-          e.val('No price found');
-        }
-      });
-    }
+    delay(function(){
+      var e = $(".cs-quantity");
+      var min = parseInt(e.attr('min'));
+      var actual = parseInt(e.val());
+      if (actual >= min) {
+        var auction_clone_id = $("#auction_clone_id").val();
+        var api_key = $('#new-auction').attr('data-key');
+        var url = '/api/auctions/'+auction_clone_id+'/best_price?quantity='+actual;
+        $.ajax({
+          type: 'GET',
+          contentType: "application/json",
+          url: url,
+          headers: {
+            'X-Spree-Token': api_key
+          },
+          success: function(data) {
+            var money_text = accounting.formatMoney((parseFloat(data)));
+            $(".cs-active-price").text(money_text);
+          },
+          error: function(data) {
+            e.val('No price found');
+          }
+        });
+      }
+    },500);
   });
 });

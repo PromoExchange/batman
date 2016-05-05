79e023a2cfc4d66ecf1beb87f9e90306e647fd48
Spencer Applegate <spencer@spencerapplegate.com>
2016-05-04 22:11:43 -0400

    Updated company store slug search to netmining

--- app/jobs/recalc_price_cache.rb
+++ app/jobs/recalc_price_cache.rb
@@ -2,7 +2,7 @@ module RecalcPriceCache
   @queue = :recalc_price_cache
 
   def self.perform
-    company_store = Spree::CompanyStore.where(slug: 'xactly').first
+    company_store = Spree::CompanyStore.where(slug: 'netmining').first
     Spree::Product.where(supplier: company_store.supplier).each do |product|
       Spree::PriceCache.where(product: product).destroy_all
       product.refresh_price_cache

0f5bdab2263015366604dcba99cd4bebe1a172be
Spencer Applegate <spencer@spencerapplegate.com>
2016-05-31 21:37:06 -0400

    Dropped blog entries table

---
+++ db/migrate/20160601013230_drop_blog_entries_table.rb
@@ -0,0 +1,11 @@
+class DropBlogEntriesTable < ActiveRecord::Migration
+  def change
+    def up
+      drop_table :spree_blog_entries
+    end
+
+    def down
+      raise ActiveRecord::IrreversibleMigration
+    end
+  end
+end

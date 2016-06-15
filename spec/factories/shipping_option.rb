101192815dd107e32a9366248b2cd90af5656086
Tim Varley <tim.varley@gmail.com>
2016-06-14 21:01:16 -0400

    Shipping option spec

--- spec/factories/shipping_option.rb
+++ spec/factories/shipping_option.rb
@@ -4,17 +4,17 @@ FactoryGirl.define do
     name 'Ship Option 1'
     delivery_date Time.zone.now + 5.days
     delivery_days 5
-    shipping_option Spree::ShippingOption::OPTION[:ups_ground]
-    shipping_cost 1.0
+
+    trait :ups_ground do
+      shipping_option Spree::ShippingOption::OPTION[:ups_ground]
+      shipping_cost 1.0
+      total_price 10.0
+    end
 
     trait :ups_3day_select do
       shipping_option Spree::ShippingOption::OPTION[:ups_3day_select]
       shipping_cost 3.0
-    end
-
-    trait :fixed_price_per_item do
-      shipping_option Spree::ShippingOption::OPTION[:fixed_price_per_item]
-      shipping_cost 4.0
+      total_price 10.0
     end
   end
 end

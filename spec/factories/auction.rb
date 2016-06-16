8ed0672ea1e57fb68c6724fc0a1b07946ce56f75
Tim Varley <tim.varley@gmail.com>
2016-06-15 20:37:55 -0400

    Spec and factory fixes, remote favorites

---
+++ spec/factories/auction.rb
@@ -0,0 +1,50 @@
+FactoryGirl.define do
+  factory :auction, class: Spree::Auction do
+    association :product
+    association :buyer, factory: :px_user
+    association :logo
+    quantity 1111
+    started 5.days.ago
+    ended nil
+    association :imprint_method
+    association :shipping_address, factory: :address
+    association :main_color, factory: :color_product
+    payment_method 'Credit Card'
+    state 'open'
+    shipping_agent 'ups'
+    ship_to_zip 30308
+    reference SecureRandom.hex(3)
+    proof_file { File.new(Rails.root.join('spec', 'fixtures', 'batman.jpeg')) }
+    association :customer, factory: :customer
+  end
+
+  # TODO: Convert to traits
+  factory :auction_with_bids, parent: :auction do
+    bids { build_list :bid, 10 }
+  end
+
+  factory :auction_with_one_bid, parent: :auction do
+    bids { build_list :bid, 1 }
+  end
+
+  factory :waiting_confirmation, parent: :auction do
+    state 'waiting_confirmation'
+  end
+
+  factory :cancelled, parent: :auction do
+    state 'cancelled'
+  end
+
+  factory :no_ref_auction, parent: :auction do
+    reference nil
+  end
+
+  factory :no_date_auction, parent: :auction do
+    started nil
+    ended nil
+  end
+
+  factory :cloned_factory, parent: :auction do
+    association :clone, factory: :auction
+  end
+end

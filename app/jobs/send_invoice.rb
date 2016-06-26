23677832de42dff02927963ace327664f2953641
Spencer Applegate <spencer@spencerapplegate.com>
2016-06-26 16:15:57 -0400

    Fixed params reference

--- app/jobs/send_invoice.rb
+++ app/jobs/send_invoice.rb
@@ -2,7 +2,7 @@ module SendInvoice
   @queue = :send_invoice
 
   def self.perform(params)
-    @auction = Spree::Auction.find(params[:auction_id])
+    @auction = Spree::Auction.find(params['auction_id'])
     SellerMailer.invoice(@auction).deliver
     BuyerMailer.invoice(@auction).deliver
   end
--- app/jobs/send_tracking_info.rb
+++ app/jobs/send_tracking_info.rb
@@ -2,7 +2,7 @@ module SendTrackingInfo
   @queue = :send_tracking_info
 
   def self.perform(params)
-    @auction = Spree::Auction.find(params[:auction_id])
+    @auction = Spree::Auction.find(params['auction_id'])
     BuyerMailer.tracking_info(@auction).deliver
   end
 end

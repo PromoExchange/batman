07fc792348f1040f8983cd1b08d9eae5a6a8e1fe
Spencer Applegate <spencer@spencerapplegate.com>
2016-06-26 09:42:15 -0400

    Flushed out send invoice job

--- app/jobs/send_invoice.rb
+++ app/jobs/send_invoice.rb
@@ -1,7 +1,9 @@
 module SendInvoice
   @queue = :send_invoice
 
-  def self.perform(params)
-    Rails.logger.info params
+  def self.perform(auction_id)
+    @auction = Spree::Auction.find(auction_id)
+    SellerMailer.invoice(@auction).deliver
+    BuyerMailer.invoice(@auction).deliver
   end
 end

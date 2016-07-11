faea85d517c27c0ef82550fa55bfffa745a93713
Tim Varley <tim.varley@gmail.com>
2016-07-11 08:06:01 -0400

    Quote calculator

--- spec/factories/shipping_option.rb
+++ spec/factories/shipping_option.rb
@@ -8,13 +8,11 @@ FactoryGirl.define do
     trait :ups_ground do
       shipping_option Spree::ShippingOption::OPTION[:ups_ground]
       shipping_cost 1.0
-      total_price 10.0
     end
 
     trait :ups_3day_select do
       shipping_option Spree::ShippingOption::OPTION[:ups_3day_select]
       shipping_cost 3.0
-      total_price 13.0
     end
   end
 end

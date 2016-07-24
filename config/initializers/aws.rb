cbdbef17c85b44ff7b11e1cd10b17b8b5b95c76b
Tim Varley <tim.varley@gmail.com>
2016-07-24 11:53:44 -0400

    Fix AWS initializer

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,7 +1,7 @@
 AWS.config({
-  access_key_id: ENV['ACCESS_KEY_ID'],
-  secret_access_key: ENV['SECRET_ACCESS_KEY'],
-  region: ENV['us-east-1'],
+  access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+  secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+  region: 'us-east-1',
 })
 
 S3_CS_BUCKET = AWS::S3.new.buckets['px-company-stores']

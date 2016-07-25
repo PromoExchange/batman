6ddacfbea9bfe5ca7f39817d9b23f3acb014f3a3
Spencer Applegate <spencer@spencerapplegate.com>
2016-07-24 21:57:27 -0400

    Ran merged migration

--- db/schema.rb
+++ db/schema.rb
@@ -520,37 +520,6 @@ ActiveRecord::Schema.define(version: 20160929164217) do
 
   add_index "spree_orders_promotions", ["order_id", "promotion_id"], name: "index_spree_orders_promotions_on_order_id_and_promotion_id", using: :btree
 
-  create_table "spree_pages", force: :cascade do |t|
-    t.string   "title"
-    t.text     "body"
-    t.string   "slug"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.boolean  "show_in_header",           default: false, null: false
-    t.boolean  "show_in_footer",           default: false, null: false
-    t.string   "foreign_link"
-    t.integer  "position",                 default: 1,     null: false
-    t.boolean  "visible",                  default: true
-    t.string   "meta_keywords"
-    t.string   "meta_description"
-    t.string   "layout"
-    t.boolean  "show_in_sidebar",          default: false, null: false
-    t.string   "meta_title"
-    t.boolean  "render_layout_as_partial", default: false
-  end
-
-  add_index "spree_pages", ["slug"], name: "index_spree_pages_on_slug", using: :btree
-
-  create_table "spree_pages_stores", id: false, force: :cascade do |t|
-    t.integer  "store_id"
-    t.integer  "page_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "spree_pages_stores", ["page_id"], name: "index_spree_pages_stores_on_page_id", using: :btree
-  add_index "spree_pages_stores", ["store_id"], name: "index_spree_pages_stores_on_store_id", using: :btree
-
   create_table "spree_payment_capture_events", force: :cascade do |t|
     t.decimal  "amount",     precision: 10, scale: 2, default: 0.0
     t.integer  "payment_id"

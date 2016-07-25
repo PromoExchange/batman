02d46803c2cc8dfa5050f8cd6d16f1062d495d5e
Spencer Applegate <spencer@spencerapplegate.com>
2016-07-24 22:52:30 -0400

    Updated upcharge templates

--- app/controllers/spree/admin/products_controller_decorator.rb
+++ app/controllers/spree/admin/products_controller_decorator.rb
@@ -1,6 +1,16 @@
 Spree::Admin::ProductsController.class_eval do
   create.before :set_custom
 
+  protected
+
+  def load_data
+    @taxons = Spree::Taxon.order(:name)
+    @option_types = Spree::OptionType.order(:name)
+    @tax_categories = Spree::TaxCategory.order(:name)
+    @shipping_categories = Spree::ShippingCategory.order(:name)
+    @suppliers = Spree::Supplier.order(:name)
+  end
+
   private
 
   def set_custom

6a93644683408cbf2e1c56d887eb5af85b9de930
Spencer Applegate <spencer@spencerapplegate.com>
2016-08-16 22:16:59 -0400

    Removed original supplier setter

--- lib/company_store_loader.rb
+++ lib/company_store_loader.rb
@@ -60,8 +60,9 @@ class CompanyStoreLoader
           name: data[:item_name],
           description: data[:product_description],
           price: 1.0,
+          original_supplier: Spree::Supplier.find(data[:supplier]).first,
           production_time: (data[:production_time] || 7),
-          supplier_id: @supplier.id,
+          supplier: @supplier,
           custom_product: true,
           color_product: []
         )

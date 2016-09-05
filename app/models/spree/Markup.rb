91faca4efc0ae37f18e2200b41a80479e3d2705d
Spencer Applegate <spencer@spencerapplegate.com>
2016-09-05 14:57:47 -0400

    Getting rid of Markup, again

--- app/models/spree/Markup.rb
+++
@@ -1,8 +0,0 @@
-class Spree::Markup < ActiveRecord::Base
-  belongs_to :supplier
-  belongs_to :company_store
-
-  validates :supplier_id, presence: true
-  validates :company_store_id, presence: true
-  validates :markup, presence: true
-end

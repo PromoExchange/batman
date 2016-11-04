0be91f181013b1e2b541640d7bdb39ae8875e4a3
Tim Varley <tim.varley@gmail.com>
2016-11-03 23:25:27 -0400

    Fix Prebid related migrations

--- db/migrate/20151216213346_add_live_to_prebid.rb
+++ db/migrate/20151216213346_add_live_to_prebid.rb
@@ -1,9 +1,11 @@
 class AddLiveToPrebid < ActiveRecord::Migration
   def change
     add_column :spree_prebids, :live, :boolean, default: false
-    Spree::Prebid.all.each do |prebid|
-      prebid.live = false
-      prebid.save!
+    if Object.const_defined?('Spree::Prebid')
+      Spree::Prebid.all.each do |prebid|
+        prebid.live = false
+        prebid.save!
+      end
     end
   end
 end
--- db/migrate/20160202113448_change_default_value_on_prebids.rb
+++ db/migrate/20160202113448_change_default_value_on_prebids.rb
@@ -2,7 +2,9 @@ class ChangeDefaultValueOnPrebids < ActiveRecord::Migration
   def change
     change_column_default :spree_prebids, :markup, nil
     change_column_default :spree_prebids, :eqp_discount, nil
-    Spree::Prebid.where(markup: 0.0).update_all(markup: nil)
-    Spree::Prebid.where(eqp_discount: 0.0).update_all(eqp_discount: nil)
+    if Object.const_defined?('Spree::Prebid')
+      Spree::Prebid.where(markup: 0.0).update_all(markup: nil)
+      Spree::Prebid.where(eqp_discount: 0.0).update_all(eqp_discount: nil)
+    end
   end
 end

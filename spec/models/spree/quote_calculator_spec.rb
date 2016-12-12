0c5d0c02e696c9fbb89855febe3489b74723e00b
Tim Varley <tim.varley@gmail.com>
2016-12-11 19:15:18 -0500

    Protect UPS call from running on codeship

--- spec/models/spree/quote_calculator_spec.rb
+++ spec/models/spree/quote_calculator_spec.rb
@@ -55,102 +55,104 @@ RSpec.describe Spree::Quote, type: :model do
     expect((quote.unit_price - 30.89).abs).to be < 0.01
   end
 
-  it 'should calculate quote with 25 quantity' do
-    quote = FactoryGirl.create(:quote, quantity: 25)
-    expect((quote.total_price - 767.33).abs).to be < 0.01
-  end
+  describe 'Can only run locally', need_ups: true do
+    it 'should calculate quote with 25 quantity' do
+      quote = FactoryGirl.create(:quote, quantity: 25)
+      expect((quote.total_price - 767.33).abs).to be < 0.01
+    end
 
-  it 'should calculate quote with 125 quantity' do
-    quote = FactoryGirl.create(:quote, quantity: 125)
-    expect((quote.total_price - 3504.79).abs).to be < 0.01
-  end
+    it 'should calculate quote with 125 quantity' do
+      quote = FactoryGirl.create(:quote, quantity: 125)
+      expect((quote.total_price - 3504.79).abs).to be < 0.01
+    end
 
-  it 'should calculate quote with 225 quantity', focus: true do
-    quote = FactoryGirl.create(:quote, quantity: 225)
-    expect((quote.total_price - 5864.37).abs).to be < 0.01
-  end
+    it 'should calculate quote with 225 quantity', focus: true do
+      quote = FactoryGirl.create(:quote, quantity: 225)
+      expect((quote.total_price - 5864.37).abs).to be < 0.01
+    end
 
-  it 'should apply product less_than_minimum charge' do
-    quote2 = FactoryGirl.create(:quote, :with_less_than_minimum)
-    expect((quote2.total_price(quantity: 75) - 842.01).abs).to be < 0.01
-    expect(quote2.total_price).to be > quote.total_price
-  end
+    it 'should apply product less_than_minimum charge' do
+      quote2 = FactoryGirl.create(:quote, :with_less_than_minimum)
+      expect((quote2.total_price(quantity: 75) - 842.01).abs).to be < 0.01
+      expect(quote2.total_price).to be > quote.total_price
+    end
 
-  it 'should apply product setup charge' do
-    quote2 = FactoryGirl.create(:quote, :with_setup_upcharges)
-    expect((quote2.total_price - 1016.28).abs).to be < 0.01
-    expect(quote2.total_price).to be > quote.total_price
-  end
+    it 'should apply product setup charge' do
+      quote2 = FactoryGirl.create(:quote, :with_setup_upcharges)
+      expect((quote2.total_price - 1016.28).abs).to be < 0.01
+      expect(quote2.total_price).to be > quote.total_price
+    end
 
-  it 'should apply product run charges' do
-    quote2 = FactoryGirl.create(:quote, :with_run_upcharges)
-    expect((quote2.total_price - 778.78).abs).to be < 0.01
-    expect(quote2.total_price).to be > quote.total_price
-  end
+    it 'should apply product run charges' do
+      quote2 = FactoryGirl.create(:quote, :with_run_upcharges)
+      expect((quote2.total_price - 778.78).abs).to be < 0.01
+      expect(quote2.total_price).to be > quote.total_price
+    end
 
-  it 'should apply product setup and run charges' do
-    quote2 = FactoryGirl.create(:quote, :with_setup_and_run_upcharges)
-    expect((quote2.total_price - 1027.73).abs).to be < 0.01
-    expect(quote2.total_price).to be > quote.total_price
-  end
+    it 'should apply product setup and run charges' do
+      quote2 = FactoryGirl.create(:quote, :with_setup_and_run_upcharges)
+      expect((quote2.total_price - 1027.73).abs).to be < 0.01
+      expect(quote2.total_price).to be > quote.total_price
+    end
 
-  it 'should apply product setup and run charges with quantity' do
-    quote2 = FactoryGirl.create(:quote, :with_setup_and_run_upcharges)
-    quote2.quantity = 125
-    expect((quote2.total_price - 3811.00).abs).to be < 0.01
-    expect(quote2.total_price).to be > quote.total_price
-  end
+    it 'should apply product setup and run charges with quantity' do
+      quote2 = FactoryGirl.create(:quote, :with_setup_and_run_upcharges)
+      quote2.quantity = 125
+      expect((quote2.total_price - 3811.00).abs).to be < 0.01
+      expect(quote2.total_price).to be > quote.total_price
+    end
 
-  it 'should apply additional location charge' do
-    quote2 = FactoryGirl.create(:quote, :with_additional_location_upcharge)
-    quote2.num_locations = 2
-    expect((quote2.total_price - 769.82).abs).to be < 0.01
-    expect(quote2.total_price).to be > quote.total_price
-  end
+    it 'should apply additional location charge' do
+      quote2 = FactoryGirl.create(:quote, :with_additional_location_upcharge)
+      quote2.num_locations = 2
+      expect((quote2.total_price - 769.82).abs).to be < 0.01
+      expect(quote2.total_price).to be > quote.total_price
+    end
 
-  it 'should apply additional location charge with quantity' do
-    quote = FactoryGirl.create(:quote, quantity: 125)
-    quote2 = FactoryGirl.create(:quote, :with_additional_location_upcharge, quantity: 125)
-    quote2.num_locations = 2
-    expect((quote2.total_price - 3514.75).abs).to be < 0.01
-    expect(quote2.total_price).to be > quote.total_price
-  end
+    it 'should apply additional location charge with quantity' do
+      quote = FactoryGirl.create(:quote, quantity: 125)
+      quote2 = FactoryGirl.create(:quote, :with_additional_location_upcharge, quantity: 125)
+      quote2.num_locations = 2
+      expect((quote2.total_price - 3514.75).abs).to be < 0.01
+      expect(quote2.total_price).to be > quote.total_price
+    end
 
-  it 'should provide fixed shipping per item' do
-    quote = FactoryGirl.create(:quote, :with_fixed_price_per_item)
-    expect((quote.total_price - 824.70).abs).to be < 0.01
-  end
+    it 'should provide fixed shipping per item' do
+      quote = FactoryGirl.create(:quote, :with_fixed_price_per_item)
+      expect((quote.total_price - 824.70).abs).to be < 0.01
+    end
 
-  it 'should provide fixed shipping total' do
-    quote = FactoryGirl.create(:quote, :with_fixed_price_total)
-    expect((quote.total_price - 871.38).abs).to be < 0.01
-  end
+    it 'should provide fixed shipping total' do
+      quote = FactoryGirl.create(:quote, :with_fixed_price_total)
+      expect((quote.total_price - 871.38).abs).to be < 0.01
+    end
 
-  # TODO: Mock UPS calls!
-  it 'should use non fixed shipping' do
-    quote = FactoryGirl.create(:quote, :with_carton)
-    expect((quote.total_price - 767.33).abs).to be < 0.01
-  end
+    # TODO: Mock UPS calls!
+    it 'should use non fixed shipping' do
+      quote = FactoryGirl.create(:quote, :with_carton)
+      expect((quote.total_price - 767.33).abs).to be < 0.01
+    end
 
-  it 'should use non fixed shipping 3day' do
-    quote = FactoryGirl.create(:quote, :with_carton)
-    expect((quote.total_price(shipping_option: :ups_3day_select) - 794.56).abs).to be < 0.01
-  end
+    it 'should use non fixed shipping 3day' do
+      quote = FactoryGirl.create(:quote, :with_carton)
+      expect((quote.total_price(shipping_option: :ups_3day_select) - 794.56).abs).to be < 0.01
+    end
 
-  it 'should use carton upcharge' do
-    quote = FactoryGirl.create(:quote, :with_carton_upcharge)
-    expect((quote.total_price - 781.16).abs).to be < 0.01
-  end
+    it 'should use carton upcharge' do
+      quote = FactoryGirl.create(:quote, :with_carton_upcharge)
+      expect((quote.total_price - 781.16).abs).to be < 0.01
+    end
 
-  it 'should use carton upcharge with multiple cartons' do
-    quote = FactoryGirl.create(:quote, :with_carton_upcharge, quantity: 250)
-    expect((quote.total_price - 6539.05).abs).to be < 0.01
-  end
+    it 'should use carton upcharge with multiple cartons' do
+      quote = FactoryGirl.create(:quote, :with_carton_upcharge, quantity: 250)
+      expect((quote.total_price - 6539.05).abs).to be < 0.01
+    end
 
-  it 'should use higher cost for express shipping' do
-    quote = FactoryGirl.create(:quote, :with_carton)
-    standard_price = quote.total_price(shipping_option: :ups_ground)
-    express_price = quote.total_price(shipping_option: :ups_next_day_air)
-    expect(express_price).to be > standard_price
+    it 'should use higher cost for express shipping' do
+      quote = FactoryGirl.create(:quote, :with_carton)
+      standard_price = quote.total_price(shipping_option: :ups_ground)
+      express_price = quote.total_price(shipping_option: :ups_next_day_air)
+      expect(express_price).to be > standard_price
+    end
   end
 end
--- spec/requests/spree/product_spec.rb
+++ spec/requests/spree/product_spec.rb
@@ -10,202 +10,204 @@ describe 'Products API' do
     expect(response).to have_http_status(401)
   end
 
-  it 'must get a best price' do
-    product = FactoryGirl.create(
-      :px_product,
-      :with_setup_upcharges,
-      :with_run_upcharges,
-      :with_carton
-    )
-    shipping_address = FactoryGirl.create(:address)
-    get "/api/products/#{product.id}/best_price",
-      {
-        quantity: 25,
-        id: product.id,
-        shipping_address: shipping_address.id,
-        shipping_option: :ups_ground
-      }, 'X-Spree-Token' => current_api_user.spree_api_key.to_s
-    expect(response).to have_http_status(200)
-    expect((json['best_price'].to_f - 841.02).abs).to be < 0.01
-    expect(json['delivery_days']).to eq 12
-  end
-
-  it 'must get a best price without parameters' do
-    product = FactoryGirl.create(:px_product)
-    get "/api/products/#{product.id}/best_price",
-      {
-      }, 'X-Spree-Token' => current_api_user.spree_api_key.to_s
-    expect(response).to have_http_status(200)
-    expect((json['best_price'].to_f - 767.33).abs).to be < 0.01
-    expect(json).not_to be_empty
-  end
-
-  xit 'must get a best price with a configuration' do
-    product = FactoryGirl.create(:px_product)
-    shipping_address = FactoryGirl.create(:address)
-    get "/api/products/#{product.id}/best_price",
-      {
-        id: product.id,
-        shipping_address: shipping_address.id,
-        shipping_option: :ups_ground
-      }, 'X-Spree-Token' => current_api_user.spree_api_key.to_s
-    expect(response).to have_http_status(200)
-    expect(json).not_to be_empty
-    expect(json.length).to eq 5
-  end
-
-  xit 'must create a product configuration' do
-    product = FactoryGirl.create(:px_product)
-    configuration = FactoryGirl.create(:preconfigure)
-    shipping_address = FactoryGirl.create(:address)
-    post "/api/products/#{product.id}/configure", {
-      imprint_method: configuration.imprint_method,
-      main_color: configuration.main_color.color,
-      custom_pms_colors: configuration.custom_pms_colors,
-      address: shipping_address.as_json
-    }, 'X-Spree-Token' => current_api_user.spree_api_key.to_s
-    expect(response).to have_http_status(200)
-  end
-
-  it 'must get a best price with quantity' do
-    product = FactoryGirl.create(
-      :px_product,
-      :with_setup_upcharges,
-      :with_run_upcharges,
-      :with_carton
-    )
-    shipping_address = FactoryGirl.create(:address)
-    get "/api/products/#{product.id}/best_price",
-      {
-        quantity: 300,
-        id: product.id,
-        shipping_address: shipping_address.id,
-        shipping_option: :ups_ground
-      }, 'X-Spree-Token' => current_api_user.spree_api_key.to_s
-    expect(response).to have_http_status(200)
-    expect((json['best_price'].to_f - 8005.10).abs).to be < 0.01
-    expect(json['delivery_days']).to eq 12
-  end
-
-  it 'must get a best price with fixed shipping per item' do
-    product = FactoryGirl.create(
-      :px_product,
-      :with_setup_upcharges,
-      :with_run_upcharges,
-      :with_fixed_price_per_item_carton
-    )
-    shipping_address = FactoryGirl.create(:address)
-    get "/api/products/#{product.id}/best_price",
-      {
-        id: product.id,
-        shipping_address: shipping_address.id,
-        shipping_option: :fixed_price_per_item
-      }, 'X-Spree-Token' => current_api_user.spree_api_key.to_s
-    expect(response).to have_http_status(200)
-    expect((json['best_price'].to_f - 898.39).abs).to be < 0.01
-    expect(json['delivery_days']).to eq 12
-  end
-
-  it 'must get a best price with upcharge carton' do
-    product = FactoryGirl.create(
-      :px_product,
-      :with_setup_upcharges,
-      :with_run_upcharges,
-      :with_upcharge_carton
-    )
-    shipping_address = FactoryGirl.create(:address)
-    product.company_store.buyer.ship_address_id = shipping_address.id
-    product.save!
-    get "/api/products/#{product.id}/best_price",
-      {
-        id: product.id,
-        purchase:
+  describe 'Can only run locally', need_ups: true do
+    it 'must get a best price' do
+      product = FactoryGirl.create(
+        :px_product,
+        :with_setup_upcharges,
+        :with_run_upcharges,
+        :with_carton
+      )
+      shipping_address = FactoryGirl.create(:address)
+      get "/api/products/#{product.id}/best_price",
         {
+          quantity: 25,
+          id: product.id,
           shipping_address: shipping_address.id,
           shipping_option: :ups_ground
-        }
-      }, 'X-Spree-Token' => current_api_user.spree_api_key.to_s
-    expect(response).to have_http_status(200)
-    expect((json['best_price'].to_f - 854.85).abs).to be < 0.01
-    expect(json['delivery_days']).to eq 12
-  end
+        }, 'X-Spree-Token' => current_api_user.spree_api_key.to_s
+      expect(response).to have_http_status(200)
+      expect((json['best_price'].to_f - 841.02).abs).to be < 0.01
+      expect(json['delivery_days']).to eq 12
+    end
 
-  it 'must get a best price with fixed shipping per item with quantity' do
-    product = FactoryGirl.create(
-      :px_product,
-      :with_setup_upcharges,
-      :with_run_upcharges,
-      :with_fixed_price_per_item_carton
-    )
-    shipping_address = FactoryGirl.create(:address)
-    get "/api/products/#{product.id}/best_price",
-      {
-        quantity: 200,
-        id: product.id,
-        shipping_address: shipping_address.id,
-        shipping_option: :fixed_price_per_item
-      }, 'X-Spree-Token' => current_api_user.spree_api_key.to_s
-    expect(response).to have_http_status(200)
-    expect((json['best_price'].to_f - 5952.72).abs).to be < 0.01
-    expect(json['delivery_days']).to eq 12
-  end
+    it 'must get a best price without parameters' do
+      product = FactoryGirl.create(:px_product)
+      get "/api/products/#{product.id}/best_price",
+        {
+        }, 'X-Spree-Token' => current_api_user.spree_api_key.to_s
+      expect(response).to have_http_status(200)
+      expect((json['best_price'].to_f - 767.33).abs).to be < 0.01
+      expect(json).not_to be_empty
+    end
 
-  it 'must get a best price with fixed shipping total' do
-    product = FactoryGirl.create(
-      :px_product,
-      :with_setup_upcharges,
-      :with_run_upcharges,
-      :with_fixed_price_total_carton
-    )
-    shipping_address = FactoryGirl.create(:address)
-    get "/api/products/#{product.id}/best_price",
-      {
-        id: product.id,
-        shipping_address: shipping_address.id,
-        shipping_option: :fixed_price_total
-      }, 'X-Spree-Token' => current_api_user.spree_api_key.to_s
-    expect(response).to have_http_status(200)
-    expect((json['best_price'].to_f - 945.07).abs).to be < 0.01
-    expect(json['delivery_days']).to eq 12
-  end
+    xit 'must get a best price with a configuration' do
+      product = FactoryGirl.create(:px_product)
+      shipping_address = FactoryGirl.create(:address)
+      get "/api/products/#{product.id}/best_price",
+        {
+          id: product.id,
+          shipping_address: shipping_address.id,
+          shipping_option: :ups_ground
+        }, 'X-Spree-Token' => current_api_user.spree_api_key.to_s
+      expect(response).to have_http_status(200)
+      expect(json).not_to be_empty
+      expect(json.length).to eq 5
+    end
 
-  it 'must get a best price with fixed shipping total with quantity' do
-    product = FactoryGirl.create(
-      :px_product,
-      :with_setup_upcharges,
-      :with_run_upcharges,
-      :with_fixed_price_total_carton
-    )
-    shipping_address = FactoryGirl.create(:address)
-    get "/api/products/#{product.id}/best_price",
-      {
-        quantity: 200,
-        id: product.id,
-        shipping_address: shipping_address.id,
-        shipping_option: :fixed_price_total
+    xit 'must create a product configuration' do
+      product = FactoryGirl.create(:px_product)
+      configuration = FactoryGirl.create(:preconfigure)
+      shipping_address = FactoryGirl.create(:address)
+      post "/api/products/#{product.id}/configure", {
+        imprint_method: configuration.imprint_method,
+        main_color: configuration.main_color.color,
+        custom_pms_colors: configuration.custom_pms_colors,
+        address: shipping_address.as_json
       }, 'X-Spree-Token' => current_api_user.spree_api_key.to_s
-    expect(response).to have_http_status(200)
-    expect((json['best_price'].to_f - 5454.82).abs).to be < 0.01
-    expect(json['delivery_days']).to eq 12
-  end
+      expect(response).to have_http_status(200)
+    end
 
-  it 'must get a best price with fixed shipping total with quantity express' do
-    product = FactoryGirl.create(
-      :px_product,
-      :with_setup_upcharges,
-      :with_run_upcharges,
-      :with_carton
-    )
-    shipping_address = FactoryGirl.create(:address)
-    get "/api/products/#{product.id}/best_price",
-      {
-        quantity: 200,
-        id: product.id,
-        shipping_address: shipping_address.id,
-        shipping_option: :ups_next_day_air
-      }, 'X-Spree-Token' => current_api_user.spree_api_key.to_s
-    expect(response).to have_http_status(200)
-    expect((json['best_price'].to_f - 5673.72).abs).to be < 0.01
-    expect(json['delivery_days']).to eq 8
+    it 'must get a best price with quantity' do
+      product = FactoryGirl.create(
+        :px_product,
+        :with_setup_upcharges,
+        :with_run_upcharges,
+        :with_carton
+      )
+      shipping_address = FactoryGirl.create(:address)
+      get "/api/products/#{product.id}/best_price",
+        {
+          quantity: 300,
+          id: product.id,
+          shipping_address: shipping_address.id,
+          shipping_option: :ups_ground
+        }, 'X-Spree-Token' => current_api_user.spree_api_key.to_s
+      expect(response).to have_http_status(200)
+      expect((json['best_price'].to_f - 8005.10).abs).to be < 0.01
+      expect(json['delivery_days']).to eq 12
+    end
+
+    it 'must get a best price with fixed shipping per item' do
+      product = FactoryGirl.create(
+        :px_product,
+        :with_setup_upcharges,
+        :with_run_upcharges,
+        :with_fixed_price_per_item_carton
+      )
+      shipping_address = FactoryGirl.create(:address)
+      get "/api/products/#{product.id}/best_price",
+        {
+          id: product.id,
+          shipping_address: shipping_address.id,
+          shipping_option: :fixed_price_per_item
+        }, 'X-Spree-Token' => current_api_user.spree_api_key.to_s
+      expect(response).to have_http_status(200)
+      expect((json['best_price'].to_f - 898.39).abs).to be < 0.01
+      expect(json['delivery_days']).to eq 12
+    end
+
+    it 'must get a best price with upcharge carton' do
+      product = FactoryGirl.create(
+        :px_product,
+        :with_setup_upcharges,
+        :with_run_upcharges,
+        :with_upcharge_carton
+      )
+      shipping_address = FactoryGirl.create(:address)
+      product.company_store.buyer.ship_address_id = shipping_address.id
+      product.save!
+      get "/api/products/#{product.id}/best_price",
+        {
+          id: product.id,
+          purchase:
+          {
+            shipping_address: shipping_address.id,
+            shipping_option: :ups_ground
+          }
+        }, 'X-Spree-Token' => current_api_user.spree_api_key.to_s
+      expect(response).to have_http_status(200)
+      expect((json['best_price'].to_f - 854.85).abs).to be < 0.01
+      expect(json['delivery_days']).to eq 12
+    end
+
+    it 'must get a best price with fixed shipping per item with quantity' do
+      product = FactoryGirl.create(
+        :px_product,
+        :with_setup_upcharges,
+        :with_run_upcharges,
+        :with_fixed_price_per_item_carton
+      )
+      shipping_address = FactoryGirl.create(:address)
+      get "/api/products/#{product.id}/best_price",
+        {
+          quantity: 200,
+          id: product.id,
+          shipping_address: shipping_address.id,
+          shipping_option: :fixed_price_per_item
+        }, 'X-Spree-Token' => current_api_user.spree_api_key.to_s
+      expect(response).to have_http_status(200)
+      expect((json['best_price'].to_f - 5952.72).abs).to be < 0.01
+      expect(json['delivery_days']).to eq 12
+    end
+
+    it 'must get a best price with fixed shipping total' do
+      product = FactoryGirl.create(
+        :px_product,
+        :with_setup_upcharges,
+        :with_run_upcharges,
+        :with_fixed_price_total_carton
+      )
+      shipping_address = FactoryGirl.create(:address)
+      get "/api/products/#{product.id}/best_price",
+        {
+          id: product.id,
+          shipping_address: shipping_address.id,
+          shipping_option: :fixed_price_total
+        }, 'X-Spree-Token' => current_api_user.spree_api_key.to_s
+      expect(response).to have_http_status(200)
+      expect((json['best_price'].to_f - 945.07).abs).to be < 0.01
+      expect(json['delivery_days']).to eq 12
+    end
+
+    it 'must get a best price with fixed shipping total with quantity' do
+      product = FactoryGirl.create(
+        :px_product,
+        :with_setup_upcharges,
+        :with_run_upcharges,
+        :with_fixed_price_total_carton
+      )
+      shipping_address = FactoryGirl.create(:address)
+      get "/api/products/#{product.id}/best_price",
+        {
+          quantity: 200,
+          id: product.id,
+          shipping_address: shipping_address.id,
+          shipping_option: :fixed_price_total
+        }, 'X-Spree-Token' => current_api_user.spree_api_key.to_s
+      expect(response).to have_http_status(200)
+      expect((json['best_price'].to_f - 5454.82).abs).to be < 0.01
+      expect(json['delivery_days']).to eq 12
+    end
+
+    it 'must get a best price with fixed shipping total with quantity express' do
+      product = FactoryGirl.create(
+        :px_product,
+        :with_setup_upcharges,
+        :with_run_upcharges,
+        :with_carton
+      )
+      shipping_address = FactoryGirl.create(:address)
+      get "/api/products/#{product.id}/best_price",
+        {
+          quantity: 200,
+          id: product.id,
+          shipping_address: shipping_address.id,
+          shipping_option: :ups_next_day_air
+        }, 'X-Spree-Token' => current_api_user.spree_api_key.to_s
+      expect(response).to have_http_status(200)
+      expect((json['best_price'].to_f - 5673.72).abs).to be < 0.01
+      expect(json['delivery_days']).to eq 8
+    end
   end
 end

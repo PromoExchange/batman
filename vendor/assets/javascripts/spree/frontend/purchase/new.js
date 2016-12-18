9b2ec10207eb81b598fa66a5dbebf771093fee87
Spencer Applegate <spencer@spencerapplegate.com>
2016-12-18 11:30:02 -0500

    Show logo in preview pane

---
+++ vendor/assets/javascripts/spree/frontend/purchase/new.js
@@ -0,0 +1,19 @@
+$(function() {
+  $(document).ready(function() {
+    $('[data-toggle="tooltip"]').tooltip();
+  });
+
+  $('.logo-upload').change(function() {
+    readURL(this);
+  });
+
+  function readURL(input) {
+    if (input.files && input.files[0]) {
+      var reader = new FileReader();
+      reader.onload = function(e) {
+        $('.logo-to-upload').attr('src', e.target.result).removeClass('hidden');
+      }
+      reader.readAsDataURL(input.files[0]);
+    }
+  }
+});

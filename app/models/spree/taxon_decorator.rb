2d63c1ceb2af881686db268bd39d294bea290b9a
Spencer Applegate <spencer@spencerapplegate.com>
2016-12-30 19:46:35 -0500

    Added images to taxon

--- app/models/spree/taxon_decorator.rb
+++ app/models/spree/taxon_decorator.rb
@@ -1,3 +1,4 @@
 Spree::Taxon.class_eval do
   has_and_belongs_to_many :option_values
+  has_many :images, -> { order(:position) }, as: :viewable, dependent: :destroy, class_name: 'Spree::Image'
 end

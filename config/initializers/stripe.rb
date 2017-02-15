03d219e0f7a0c360292b7cd74eda27a71c8025a7
Tim Varley <tim.varley@gmail.com>
2017-02-15 14:19:32 -0500

    Check Stripe location setting during initialization

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -3,4 +3,13 @@ Rails.configuration.stripe = {
   secret_key:      ENV['STRIPE_SECRET_KEY']
 }
 
+pk_env = Rails.configuration.stripe[:publishable_key][3..6]
+sk_env = Rails.configuration.stripe[:secret_key][3..6]
+if ( pk_env == 'test' || sk_env == 'test' )
+  raise "Attempting to run TEST stripe in production" if Rails.env.production?
+else
+  raise "Attempting to run LIVE stripe in non production" unless Rails.env.production?
+end
+raise "Both Stripe settings must point to same location" if sk_env != pk_env
+
 Stripe.api_key = Rails.configuration.stripe[:secret_key]

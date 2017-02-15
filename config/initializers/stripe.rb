420555b5610f712be123b6d8a21ed12d7b35001c
Tim Varley <tim.varley@gmail.com>
2017-02-15 17:37:32 -0500

    Adjust logic to be a little safer, live has to be explicit, not implied

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -3,12 +3,12 @@ Rails.configuration.stripe = {
   secret_key:      ENV['STRIPE_SECRET_KEY']
 }
 
-pk_env = Rails.configuration.stripe[:publishable_key][3..6]
-sk_env = Rails.configuration.stripe[:secret_key][3..6]
-if ( pk_env == 'test' || sk_env == 'test' )
-  raise "Attempting to run TEST stripe in production" if Rails.env.production?
-else
+pk_env = Rails.configuration.stripe[:publishable_key] && Rails.configuration.stripe[:publishable_key][3..6]
+sk_env = Rails.configuration.stripe[:secret_key] && Rails.configuration.stripe[:secret_key][3..6]
+if ( pk_env == 'live' || sk_env == 'live' )
   raise "Attempting to run LIVE stripe in non production" unless Rails.env.production?
+else
+  raise "Attempting to run TEST stripe in production" if pk_env && sk_env && Rails.env.production?
 end
 raise "Both Stripe settings must point to same location" if sk_env != pk_env
 
